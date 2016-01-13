4d6752d21c4c1a6ea8c1e143a2335f2bc1426999
SteffenBerlin <Steffen.Baumgarten@gmx.de>
2016-01-13 15:55:23 -0800

    Implement Functional-Class Stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = { stack: {}, stackSize: 0 };
+  _.extend(obj, stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	push: function(value) {
+		this.stack[this.stackSize] = value;
+		this.stackSize++;
+	},
 
+	pop: function() {
+		if (this.stackSize > 0) {
+			this.stackSize--;
+			return this.stack[this.stackSize];
+		}
+		return null;
+	},
+
+	size: function() { return this.stackSize; }
+};
 
