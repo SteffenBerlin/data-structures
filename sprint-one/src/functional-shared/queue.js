284e6932ec6b2019e5c0e8c82084d046cc0d949c
SteffenBerlin <Steffen.Baumgarten@gmx.de>
2016-01-13 16:07:53 -0800

    Implement Functional-Class Queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {
+  	queue: {},
+  	start: 0,
+  	end : 0
+  };
+  _.extend(obj, queueMethods);
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+	enqueue: function(value){
+		this.queue[this.end]=value;
+		this.end++;
+	},
+
+	dequeue: function(){
+		if(this.end > this.start){
+			this.start++;
+			return(this.queue[this.start-1]);
+		}
+	},
+
+	size: function(){
+		return this.end - this.start;
+	}
+};
 
 
