db0a3f1d645e2d798b5f309d382e87d9c13eca93
SteffenBerlin <Steffen.Baumgarten@gmx.de>
2016-01-13 18:38:36 -0800

    begin prototypal

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(Stack.stackMethods);
+  
+  obj.size = 0;
+  obj.stack = {};
+  
+  return obj;
 };
 
-var stackMethods = {};
+Stack.stackMethods.push = function(value) {
+	this.stack[this.size] = value;
+	this.size++;
+};
 
+Stack.stackMethods.pop = function() {
+	if (this.size > 0) {
+		this.size--;
+		return this.stack[this.size+1];
+	}
+	return null;
+};
 
+Stack.stackMethods.size = function() {
+	return this.size;
+};
