bc6f128f669c5ea6e7d11bcfdd386f896c54c574
SteffenBerlin <Steffen.Baumgarten@gmx.de>
2016-01-15 11:48:03 -0800

    created binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,50 @@
 var BinarySearchTree = function(value) {
+	var binarySearchTree = {};
+	binarySearchTree.left = null;
+	binarySearchTree.right = null;
+	binarySearchTree.value = value;
+
+	binarySearchTree.insert = function(value) {
+		if (binarySearchTree.value < value) {
+			if (binarySearchTree.right === null) {
+				binarySearchTree.right = BinarySearchTree(value);
+			} else {
+				binarySearchTree.right.insert(value);
+			}
+		}
+		else {
+			if (binarySearchTree.left === null) {
+				binarySearchTree.left = BinarySearchTree(value);
+			} else {
+				binarySearchTree.left.insert(value);
+			}
+		}
+	};
+
+	binarySearchTree.contains = function(value) {
+		if(binarySearchTree.value === value){
+			return true;
+		}
+		else if (binarySearchTree.value < value && binarySearchTree.right){
+			return binarySearchTree.right.contains(value);
+		}
+		else if (binarySearchTree.value > value && binarySearchTree.left){
+			return binarySearchTree.left.contains(value);
+		}	
+		return false;
+	};
+
+	binarySearchTree.depthFirstLog= function(cb) {
+		cb(binarySearchTree.value);
+		if (binarySearchTree.left){
+			binarySearchTree.left.depthFirstLog(cb);
+		}
+		if (binarySearchTree.right) {
+			binarySearchTree.right.depthFirstLog(cb);
+		}
+	};
+
+	return binarySearchTree;
 };
 
 
