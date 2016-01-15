a7e8e8bf2f2452afc9a82956c7e2aaba9f145217
SteffenBerlin <Steffen.Baumgarten@gmx.de>
2016-01-14 19:28:33 -0800

    Implement prototypal set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,15 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+	this[item] = item;
 };
 
 setPrototype.contains = function(item) {
+	return item in this;
 };
 
 setPrototype.remove = function(item) {
+	delete this[item];
 };
 
 /*
