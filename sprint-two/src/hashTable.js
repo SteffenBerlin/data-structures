ba788f3de33da2cfac12355619eee1e8b25f547b
SteffenBerlin <Steffen.Baumgarten@gmx.de>
2016-01-15 12:24:28 -0800

    Implement double and halve method

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,39 +2,82 @@
 
 var HashTable = function() {
   this._limit = 8;
+  this._count = 0;
   this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var objAtIndex = this._storage.get(index);
-  //console.log(objAtIndex);
+  this._count++;
+
+  if (this._count/this._limit >= 0.75) {
+    this.double();
+  }
+
   if (objAtIndex) {
-  	//console.log("check");
-	objAtIndex[k] = v;
-	this._storage.set(index, objAtIndex);
+  	objAtIndex[k] = v;
+  	this._storage.set(index, objAtIndex);
   }
   else {
   	var obj = {};
   	obj[k] = v;
-	this._storage.set(index, obj);
+    this._storage.set(index, obj);
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var objAtIndex = this._storage.get(index);
-  console.log(objAtIndex);
+  
   return objAtIndex[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var objAtIndex = this._storage.get(index);
+  this._count--;
   delete objAtIndex[k];
   this._storage.set(index, objAtIndex);
+
+  if (this._count/this._limit <= 0.25 && this._limit > 8) {
+    this.halve();
+  }
 };
 
+HashTable.prototype.double = function() {
+  this._limit *= 2;
+  this._count = 0;
+
+  var oldLimitedArray = this._storage;
+  this._storage = LimitedArray(this._limit);
+
+  for(var i = 0; i < this._limit/2; i++) {
+    var obj = oldLimitedArray.get(i);
+    if (obj) {
+      for(var key in obj) {
+        this.insert(key, obj[key]);
+      }
+    }
+  }
+};
+
+HashTable.prototype.halve = function() {
+  this._limit /= 2;
+  this._count = 0;
+  
+  var oldLimitedArray = this._storage;
+  this._storage = LimitedArray(this._limit);
+
+  for(var i = 0; i < this._limit*2; i++) {
+    var obj = oldLimitedArray.get(i);
+    if (obj) {
+      for(var key in obj) {
+        this.insert(key, obj[key]);
+      }
+    }
+  }
+};
 
 
 /*
