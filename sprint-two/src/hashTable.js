20cf2fa2ef0de51574e4fd406d1855382529f544
SteffenBerlin <Steffen.Baumgarten@gmx.de>
2016-01-14 20:31:43 -0800

    create hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,32 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var objAtIndex = this._storage.get(index);
+  //console.log(objAtIndex);
+  if (objAtIndex) {
+  	//console.log("check");
+	objAtIndex[k] = v;
+	this._storage.set(index, objAtIndex);
+  }
+  else {
+  	var obj = {};
+  	obj[k] = v;
+	this._storage.set(index, obj);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var objAtIndex = this._storage.get(index);
+  console.log(objAtIndex);
+  return objAtIndex[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var objAtIndex = this._storage.get(index);
+  delete objAtIndex[k];
+  this._storage.set(index, objAtIndex);
 };
 
 
